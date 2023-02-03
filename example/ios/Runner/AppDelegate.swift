b0322804c73263208fdf348ca0f065795953f051
Gonza <gonzalo.gauto@gmail.com>
2023-02-03 09:48:49 -0300

    update app example

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,11 +3,26 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+  var navigationController: UINavigationController?;
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Add this line before GeneratedPluginRegistrant
+    let flutterViewController: FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    // This line is added by the Flutter App Generator
     GeneratedPluginRegistrant.register(with: self)
+
+    // Add these lines after GeneratedPluginRegistrant
+    self.navigationController = UINavigationController(rootViewController: flutterViewController);
+    self.navigationController?.setNavigationBarHidden(true, animated: false);
+
+    self.window = UIWindow(frame: UIScreen.main.bounds);
+    self.window.rootViewController = self.navigationController;
+    self.window.makeKeyAndVisible();
+    // End of edit
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
